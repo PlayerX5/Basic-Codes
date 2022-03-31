1e49fd8e0741672434dac3f7bd0b3b8981ad949b
PlayerX5 <102201389+PlayerX5@users.noreply.github.com>
2022-03-31 20:36:44 +0530

    Create CheckPrime.c

---
+++ CheckPrime.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main()
+{
+    int n, i, count=0;
+    printf("Enter no:\n");
+    scanf("%d", &n);
+    for(i=1; i<=n; i++)
+    {
+        if(n%i==0)
+        {
+            count++;
+        }
+    }
+    if(count==2)
+    {
+        printf("Prime no.\n");
+    }
+    else
+    {
+        printf("Not a Prime no.\n");
+    }
+
+    return 0;
+}
