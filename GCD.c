c8f7f2a545161ce30d821138f0b5a8425bd1b5f4
PlayerX5 <102201389+PlayerX5@users.noreply.github.com>
2022-03-31 20:09:53 +0530

    Create GCD.c

---
+++ GCD.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int gcd(int x,int y);
+int main()
+{
+  int num1,num2,hcf,lcm;
+  printf("Enter two integer values:\n");
+  scanf("%d%d",&num1,&num2);
+  hcf=gcd(num1,num2);
+  printf("GCD:%d\n",hcf);
+  printf("LCM:%d\n",(num1*num2)/hcf);
+  return 0;
+}
+int gcd(int x, int y)
+{
+  if(y==0)
+  {
+    return x;
+  }
+  else
+  {
+    return gcd(y,x%y);
+  }
+}
